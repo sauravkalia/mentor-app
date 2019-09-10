94118d5c9cfc3f6e6c1dc440c8dc873e95350983
saurav kalia <sauravhunny@gmail.com>
2019-09-10 19:26:46 +0530

    updates to deploy to heroku

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -13,4 +14,4 @@ res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080); 
