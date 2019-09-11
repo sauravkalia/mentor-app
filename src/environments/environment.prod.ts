683fd9b91f36b5120f3537073da4713e154290fd
saurav kalia <sauravhunny@gmail.com>
2019-09-11 13:38:52 +0530

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCvibU5JlDnwy26dUikaCrwDG2wBYcdCFY',
+    authDomain: 'mentoring-app-63b39.firebaseapp.com',
+    databaseURL: 'https://mentoring-app-63b39.firebaseio.com',
+    projectId: 'mentoring-app-63b39',
+    storageBucket: '',
+    messagingSenderId: '490357902694',
+    appId: '1:490357902694:web:d7b814850b506a70'
+  }
 };
